cbd9d5a9a3cd0fa8c2f461dfe0d5c4f7d2f00533
mohammedpythons <mohammedshokai99@gmail.com>
2023-09-07 08:42:36 -0400

    fix sending request from cutomer with @LoadBalanced

--- CustomerService/src/main/java/com/moe/CustomerService/service/CustomerRegistrationService.java
+++ CustomerService/src/main/java/com/moe/CustomerService/service/CustomerRegistrationService.java
@@ -24,7 +24,7 @@ public class CustomerRegistrationService {
         customerRepository.saveAndFlush(customer);
 //        todo: check if fraudster
         FraudCheckResponse fraudCheckresponse =  restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
                 );
