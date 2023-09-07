e98e743485f972b84ef4f1b4948bddc4a73372d8
mohammedpythons <mohammedshokai99@gmail.com>
2023-09-07 07:52:43 -0400

    connect clients to eureka server

--- CustomerService/src/main/java/com/moe/CustomerService/CustomerServiceApplication.java
+++ CustomerService/src/main/java/com/moe/CustomerService/CustomerServiceApplication.java
@@ -3,8 +3,10 @@ package com.moe.CustomerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
